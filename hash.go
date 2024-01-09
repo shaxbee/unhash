fb60fa78a5f96b0735348b9a98e46966b5781551
Zibi Mandziejewicz <zibi.mandziejewicz@agoda.com>
2024-01-09 21:15:19 +0700

    doc: HashMap docs

--- hash.go
+++ hash.go
@@ -6,6 +6,7 @@ import (
 	"github.com/shaxbee/unhash/internal/fasthash/fnv1"
 )
 
+// MaxDepthError indicates value that is nested above max depth
 type MaxDepthError struct {
 	Path string
 }
@@ -14,6 +15,7 @@ func (e MaxDepthError) Error() string {
 	return fmt.Sprintf("unhash: max depth reached at %s", e.Path)
 }
 
+// InvalidValueTypeError
 type InvalidTypeError struct {
 	Path string
 	Type string
@@ -23,6 +25,15 @@ func (e InvalidTypeError) Error() string {
 	return fmt.Sprintf("unhash: unsupported type %s", e.Type)
 }
 
+// HashMap computes a hash of unstructured object.
+//
+// Supported values:
+//   - string
+//   - int64
+//   - float64
+//   - bool
+//   - map[string]any
+//   - []any
 func HashMap(data map[string]any, config Config) (uint64, error) {
 	config = ConfigDefault(config)
 
